7aeca39fe5d3dbe32ee6e482946cece47c35cdcf
Zhou <alezhou@paypal.com>
2019-05-23 14:19:10 +0800

    demo for car

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const chalk = require('chalk');
+const ip = require('ip');
+const divider = chalk.gray('\n-----------------------------------');
+const host = 'localhost';
+const port = 4000;
+
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
   res.sendFile( __dirname + "/public/index.html" );
 });
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + '/public'));
 
-app.listen(4000, () => console.log("Server listening on port 4000, open browser at 'localhost:4000'"));
+var log_text = `${chalk.bold('Access URLs:')}${divider}
+  Localhost: ${chalk.magenta(`http://${host}:${port}`)}
+  LAN: ${chalk.magenta(`http://${ip.address()}:${port}`)}
+  Access the LAN address at Magic Leap Helio Browser
+  ${divider}
+${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}`;
+
+app.listen(port, () => console.log(log_text) );
